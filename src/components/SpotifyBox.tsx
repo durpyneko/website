1f624eac21f3c1597a6de85139f8bbdf50fb7cb7
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2024-01-01 19:16:21 +0200

    integrate lanyard api
    update avatar, status and currently playing spotify track with https://lanyard.rest/

---
+++ src/components/SpotifyBox.tsx
@@ -0,0 +1,59 @@
+// Chakra
+import { Box, Image, VStack, Text, Link } from "@chakra-ui/react";
+
+// Interface
+interface SpotifyBoxProps {
+  cover: string;
+  title: string;
+  artist: string;
+  track_id: string;
+}
+
+export default function SpotifyBox({
+  cover,
+  title,
+  artist,
+  track_id,
+}: SpotifyBoxProps) {
+  return (
+    <Box
+      backgroundColor="#332e45"
+      width={{ base: "80vw", md: "360px" }}
+      maxW={{ base: "80vw", md: "360px" }}
+      borderRadius="8px"
+      display="flex"
+      alignItems="center"
+      padding="10px"
+    >
+      {cover ? (
+        <>
+          <Image
+            src={cover}
+            alt="spotify"
+            width="60px"
+            height="60px"
+            borderRadius="50%"
+            objectFit="cover"
+          />
+          <VStack marginLeft="10px" alignItems="flex-start">
+            <Link
+              href={"https://open.spotify.com/track/" + track_id}
+              target={"_blank"}
+            >
+              <Text fontSize="16px" fontWeight="bold" noOfLines={1}>
+                {title}
+              </Text>
+            </Link>
+            <Text fontSize="14px" noOfLines={1}>
+              {artist}
+            </Text>
+          </VStack>
+        </>
+      ) : (
+        <Text fontSize="16px" color="white">
+          Nothing is currently playing
+        </Text>
+      )}
+    </Box>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,86 @@
-// Chakra Imports
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+
+// Chakra
 import { Box, Center, Text, HStack, Image, Flex } from "@chakra-ui/react";
 
 // Components
 import Header from "@/components/Header";
 import TechStack from "@/components/TechStack";
 
+// Lib
+import fetcher from "@/lib/fetcher";
+
+// Interface
+interface UserData {
+  data: {
+    discord_user: {
+      avatar: string;
+    };
+    spotify: {
+      song: string;
+      artist: string;
+      album_art_url: string;
+      track_id: string;
+    };
+    discord_status: string;
+  };
+}
+
 // Icons
 import { FaMusic } from "react-icons/fa";
-import {
-  SiTypescript,
-  SiJavascript,
-  SiRust,
-  SiPython,
-  SiNextdotjs,
-} from "react-icons/si";
 import { LuCode2 } from "react-icons/lu";
-import { FaReact } from "react-icons/fa";
 
 // Fonts
 import { JetBrains_Mono } from "next/font/google";
+import SpotifyBox from "@/components/SpotifyBox";
 const JetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export default function Index() {
+  const [title, setTitle] = useState<string>("loading...");
+  const [artist, setArtist] = useState<string>("loading...");
+  const [cover, setCover] = useState<string>("loading...");
+  const [track_id, setTrack_id] = useState<string>("loading...");
+  const [dc_status, setDc_status] = useState<string>("loading...");
+  const [avatar, setAvatar] = useState<string>("loading...");
+
+  const { data, error } = useSWR<UserData>(
+    "https://api.lanyard.rest/v1/users/763864687481323620",
+    fetcher,
+    {
+      refreshInterval: 60000,
+      revalidateOnFocus: true,
+    }
+  );
+
+  useEffect(() => {
+    if (data) {
+      setTitle(data.data.spotify?.song);
+      setArtist(data.data.spotify?.artist);
+      setCover(data.data.spotify?.album_art_url);
+      setTrack_id(data.data.spotify?.track_id);
+      setDc_status(data.data.discord_status);
+      setAvatar(data.data.discord_user.avatar);
+    }
+  }, [data]);
+
   return (
     <>
       <Header header="Home" />
       <Center>
         <Box
-          w={{ base: "100vw", md: "70vw" }} // Set to 100vw on smaller screens
+          w={{ base: "100vw", md: "70vw" }}
           p={"20px"}
           className={`${JetBrainsMono.className}`}
         >
           <HStack>
             <Image
               style={{ borderRadius: "50%" }}
-              src="/profile.png"
+              src={
+                "https://cdn.discordapp.com/avatars/763864687481323620/" +
+                avatar +
+                ".png?size=1024"
+              }
               boxSize={"55px"}
               placeholder="blur"
               loading="lazy"
@@ -55,6 +101,7 @@ export default function Index() {
               </Text>
               !
             </Text>
+            <Text fontSize="xl">{dc_status}</Text>
           </HStack>
           <Flex
             maxW={"6xl"}
@@ -90,6 +137,14 @@ export default function Index() {
               <Text>• DnB, Breakcore</Text>
               <Text>• Techno</Text>
             </Box>
+          </Box>
+          <Box pt={"20px"}>
+            <SpotifyBox
+              cover={cover}
+              title={title}
+              artist={artist}
+              track_id={track_id}
+            />
           </Box>
         </Box>
       </Center>
